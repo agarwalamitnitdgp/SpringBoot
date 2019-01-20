11fcb2dbf59583b39735a6d5907a74f41c951d0a
agarwalamitnitdgp <agarwalamit.nitdgp@gmail.com>
2019-01-20 19:47:19 +0530

    Changes for Spring Data Rest Which is also included in the POM file

--- SpringRestStarter/src/main/java/com/rest/respository/PostRepository.java
+++ SpringRestStarter/src/main/java/com/rest/respository/PostRepository.java
@@ -3,6 +3,7 @@ package com.rest.respository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rest.domain.Post;
@@ -15,5 +16,7 @@ public interface PostRepository extends CrudRepository<Post, Long>{
 	List<Post> findAllByOrderByPostedOnDesc();
 
 	Post findBySlug(String slug);
-
+	
+	List<Post> findByTitleContaining(@Param("title") String title); //  http://localhost:8080/posts/search/findByTitleContaining?title=Spring%20Boot%20Rocks! This URL can be used for it
+ 
 }
